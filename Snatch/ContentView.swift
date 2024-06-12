0c81b8cd8e536efe3eb9964e1dee1d436d0c7b2a
perceval9 <perceval@umich.edu>
2024-06-13 01:26:48 +0200

    new app in swift

--- Snatch/ContentView.swift
+++ Snatch/ContentView.swift
@@ -3,7 +3,7 @@
 //  Snatch
 //
 //  Created by Perceval Park on 6/11/24.
-//
+//  Updated by Perceval Park on 6/13/24.
 
 import SwiftUI
 
@@ -14,21 +14,53 @@ struct ContentView: View {
             Color(.white)
                 .ignoresSafeArea()
             
-            VStack(alignment: .leading) { //vertical stack
-                Image("unnamed")
-                    .resizable()
-                    .cornerRadius(15)
-                    .aspectRatio(contentMode: .fit)
-                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                HStack {
-                    Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
-                        .font(.largeTitle)
-                        .fontWeight(.semibold)
-                        .foregroundColor(Color.black)
-                    Image(systemName: "star")
-                }
+            VStack(alignment: .leading) {
+                Text("Tutorial")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
                 
-
+                VStack(alignment: .leading) { //vertical stack
+                    Image("snatchpic")
+                        .resizable()
+                        .cornerRadius(15)
+                        .aspectRatio(contentMode: .fit)
+                        .padding()
+                    HStack {
+                        Text("The Snatch")
+                            .font(.largeTitle)
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.black)
+                        
+                        
+                        Image(systemName: "star")
+                        
+                        Spacer()
+                        Button("Begin") {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        } .buttonStyle(.bordered)
+                        Spacer()
+                    }
+                    
+                    
+                    Text("Bar starts on the ground in the wide grip position. Bar moves from the ground directly to the overhead position without stopping. Lifter may land in the squat or the power position before standing tall.")
+                        .font(.caption)
+                    
+                    
+                    
+                    
+                    
+                }
+                .padding()
+                .background(Rectangle()
+                    .cornerRadius(15)
+                    .foregroundColor(.white)
+                    .shadow(radius: 15))
+                
+                
+                .padding()
+                
+                
             }
             
         }
